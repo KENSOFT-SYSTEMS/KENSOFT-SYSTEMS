751daa4455c44563cb3b98c0128edd7190f2fa25
Kenneth Nyaga <info@kensoftwares.co.ke>
2025-05-09 18:18:00 +0300

    Add files via upload

---
+++ includes/db.php
@@ -0,0 +1,6 @@
+<?php
+$conn = new mysqli("localhost", "qnrebcbs_nyumbani", "gYNgMj@s@y{G", "qnrebcbs_nyumbani");
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+?>
